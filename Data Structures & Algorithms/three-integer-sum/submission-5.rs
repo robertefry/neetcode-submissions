31104bf74b88421a9e02742b1b80a50b7c82e868
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-07 12:48:00 +0100

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.rs
@@ -0,0 +1,37 @@
+
+use std::{cmp::Ordering, collections::*};
+
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>>
+    {
+        nums.sort_unstable();
+
+        let n = nums.len();
+        let mut result = Vec::new();
+
+        for i in 0..n
+        {
+            if i > 0 && nums[i] == nums[i-1] {
+                continue;
+            }
+            let (mut l, mut r) = (i+1, n-1);
+
+            while l < r {
+                match (nums[i] + nums[l] + nums[r]).cmp(&0) {
+                    Ordering::Equal => {
+                        result.push(vec![nums[i],nums[l],nums[r]]);
+                        while { l += 1; l < r && nums[l] == nums[l-1] }{}
+                    },
+                    Ordering::Less => {
+                        while { l += 1; l < r && nums[l] == nums[l-1] }{}
+                    },
+                    Ordering::Greater => {
+                        while { r -= 1; r > l && nums[r] == nums[r+1] }{}
+                    },
+                }
+            }
+        }
+
+        return result;
+    }
+}
