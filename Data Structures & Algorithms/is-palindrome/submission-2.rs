d4c70a9059081caddc7131d82d3d6026249a1ac5
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-07 11:38:33 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.rs
@@ -0,0 +1,13 @@
+
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool
+    {
+        let chrs: Vec<_> = s.chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+        let n = chrs.len();
+
+        (0..n>>1).all(|i| chrs[i] == chrs[n-i-1])
+    }
+}
