0d214af8254a0895b329b79817fdda9fa6bd7d02
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-08 20:13:40 +0100

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.rs
@@ -0,0 +1,31 @@
+
+impl Solution {
+    pub fn combination_sum(mut nums: Vec<i32>, target: i32) -> Vec<Vec<i32>>
+    {
+        fn combination_sum(nums: &[i32], i: usize, target: i32) -> Vec<Vec<i32>>
+        {
+            if i >= nums.len() || nums[i] > target {
+                return if target == 0 { vec![vec![]] } else { Vec::new() }
+            }
+
+            let mut result = Vec::new();
+
+            for taken in 0.. {
+                if taken * nums[i] > target {
+                    break;
+                }
+
+                let partials = combination_sum(nums, i+1, target - taken * nums[i]);
+
+                for mut partial in partials {
+                    partial.append(&mut vec![nums[i]; taken as usize]);
+                    result.push(partial);
+                }
+            }
+
+            return result;
+        }
+        nums.sort_unstable();
+        combination_sum(&nums, 0, target)
+    }
+}
