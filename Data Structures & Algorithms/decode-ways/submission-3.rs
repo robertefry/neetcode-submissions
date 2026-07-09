2d53c82704b3523b0c4ce06224fbc23e9c91c663
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-09 23:31:09 +0100

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.rs
@@ -0,0 +1,35 @@
+
+impl Solution {
+    pub fn num_decodings(s: String) -> i32
+    {
+        fn num_unit_encodings(chars: &[char]) -> i32 {
+            if match chars {
+                ['1'..='9'] => true,
+                ['1', '0'..='9'] => true,
+                ['2', '0'..='6'] => true,
+                _ => false,
+            } { 1 } else { 0 }
+        }
+
+        let n = s.len();
+        let chars: Vec<_> = s.chars().collect();
+
+        let mut counts = vec![0;n];
+        if n >= 1 {
+            counts[n-1] += num_unit_encodings(&chars[n-1..]);
+        }
+        if n >= 2 {
+            counts[n-2] += num_unit_encodings(&chars[n-2..]);
+            counts[n-2] += num_unit_encodings(&chars[n-2..n-1]) * counts[n-1];
+        }
+
+        for i in (0..n.saturating_sub(2)).rev() {
+            counts[i] += num_unit_encodings(&chars[i..i+1]) * counts[i+1];
+            counts[i] += num_unit_encodings(&chars[i..i+2]) * counts[i+2];
+        }
+
+        println!("num_unit_encodings(\"34\") = {}", num_unit_encodings(&vec!['3','4']));
+        println!("{counts:?}");
+        counts[0]
+    }
+}
