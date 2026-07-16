76711ccb9b799bb16f781de459f66d845e262cc9
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-16 18:25:29 +0100

    Add: counting-bits - submission-4

---
+++ Data Structures & Algorithms/counting-bits/submission-4.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    vector<int> countBits(int n)
+    {
+        auto result = vector<int>(n+2);
+        result[1] = 1;
+
+        for (size_t i = 1; i <= n/2; ++i) {
+            result[2*i+0] = result[i];
+            result[2*i+1] = result[i] + 1;
+        }
+        result.pop_back();
+
+        return result;
+    }
+};
