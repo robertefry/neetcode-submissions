a08bb31b42704b6827173b97f881220debaaee14
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-17 18:21:37 +0100

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q)
+    {
+        auto nodes = vector<tuple<TreeNode*,TreeNode*>>{{p,q}};
+
+        while (not nodes.empty())
+        {
+            auto const [p,q] = nodes.back();
+            nodes.pop_back();
+
+            if ((p == nullptr) != (q == nullptr)) {
+                return false;
+            }
+            if (p == nullptr) {
+                continue;
+            }
+
+            if (p->val != q->val) {
+                return false;
+            }
+
+            nodes.push_back({p->left,q->left});
+            nodes.push_back({p->right,q->right});
+        }
+
+        return true;
+    }
+};
