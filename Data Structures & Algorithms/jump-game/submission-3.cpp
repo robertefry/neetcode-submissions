991f8f8e790a684e92819c88bd68f50fb02c5776
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-22 01:13:33 +0100

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums)
+    {
+        if (nums.empty()) {
+            return true; // vacuous truth
+        }
+
+        auto dp = vector<bool>(nums.size(),false);
+        dp[0] = true;
+
+        for (auto i = 0; i < nums.size(); ++i)
+            if (dp[i])
+                for (auto j = 1; j <= nums[i] && j < nums.size()-i; ++j)
+        {
+            dp[i+j] = true;
+        }
+
+        return dp[nums.size()-1];
+    }
+};
