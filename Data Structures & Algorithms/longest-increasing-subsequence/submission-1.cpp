77576cfa654d4a865f314b42b1f58c543942a0cb
Robert Fry <43712054+robertefry@users.noreply.github.com>
2026-07-30 22:55:13 +0100

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        auto dp = vector<int>(nums.size(),1);
+
+        for (size_t i = nums.size()-1; i < nums.size(); --i)
+            for (size_t j = i+1; j < nums.size(); ++j)
+        {
+            if (nums[j] <= nums[i]) {
+                continue;
+            }
+            if (dp[i] > nums.size() - j) {
+                // here, even if j..n is increasing meaning nums[j] is maximised,
+                // nums[i] is already larger. so there's no point continuing.
+                break;
+            }
+            dp[i] = max(dp[i], 1 + dp[j]);
+        }
+
+        return *std::max_element(dp.begin(), dp.end());
+    }
+};
